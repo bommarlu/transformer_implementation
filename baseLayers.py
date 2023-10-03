added test to optimize a fully connected layer with softmax. Given this works, there is a bug in the attention class.

baseLayers.py
@@ -125,7 +125,7 @@ class FullyConnectedLayer(Layer):
         if not np.any(starting_weights_in):
             self.set_weights(starting_weights=np.ones(self.weights_shape))
         else:
-            self.set_weights(starting_weights=np.ones(self.weights_shape))
+            self.set_weights(starting_weights=starting_weights_in)
 
     '''
     set_learning_rate:

testing/baseLayers/testBaseLayers.py
@@ -57,8 +57,27 @@ def test_softmax():
         print(soft_out)
         print(np.sum(np.power(soft_out, 2)))
 
+def test_fc_with_softmax():
+    weights = np.random.rand(4, 2)
+    new_layer = FullyConnectedLayer(4, 2, starting_weights_in=weights)
+
+    softmax = SoftmaxLayer(axis=1)
+
+    data_in = np.array([[1,1,1,1],
+                        [2,2,2,3]])
+    loss = None
+    while not loss or loss > 1.001:
+        soft_in = new_layer.forward(data_in)
+        soft_out = softmax.forward(soft_in)
+        
+        soft_out_back = softmax.backward(2*soft_out)
+        new_layer.backward(soft_out_back)
+        soft_in = soft_in - soft_out_back * 0.5
+        print(soft_out)
+        print(np.sum(np.power(soft_out, 2)))
+        loss = np.sum(np.power(soft_out, 2))
 # test_forward_name()
 # test_set_get_weights()
 # test_backward_fc()
 
-test_softmax()
+test_fc_with_softmax()

testing/selfAttention.py/testAttentionHead.py
@@ -6,35 +6,10 @@ import numpy as np
 
 
 
-def test_attention_backward_trivial():
-    def loss_function(output):
-        return np.sum(np.power(output, 2))
-    input_data = np.array([[1, 1, 1, 1],
-                            [1, 1,1, 1]])
-    attention = AttentionLayer(sequence_length_in=2, token_length_in=4)
-    loss = None
-    while not loss or loss > 0.5:
-        logger.info('============= FORWARD PASS =============')
-        forward_pass = attention.forward(input_data)
-        loss = loss_function(forward_pass)
-
-        logger.info(f'forward_pass result:\n{forward_pass}')
-        logger.info(f'loss:{loss}')
-
-        logger.info('============= BACKWARD PASS =============')
-        loss_grad = 2 * forward_pass
-        logger.info(f'output_grad:\n{loss_grad}')
-        backward_pass = attention.backward(loss_grad)
-        logger.info(f'backward_pass query:\n{backward_pass[0]}')
-        logger.info(f'backward_pass keys:\n{backward_pass[1]}' )
-
-
-
 def test_attention_backward():
     def loss_function(output):
-        return np.linalg.norm(output, ord=2)
-    input_data = np.array([[1, 2, 3, 4],
-                           [5, 5, 5, 5]])
+        return np.sum(np.power(output, 2))
+    input_data = np.random.rand(2,4)
     attention = AttentionLayer(sequence_length_in=2, token_length_in=4)
     loss = None
     while not loss or loss > 1: